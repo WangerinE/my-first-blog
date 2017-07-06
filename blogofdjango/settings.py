5d3b5144a985f298f5f8b75edb9ef52c4571713b
Wangerin <elliewangerine@gmail.com>
2017-07-06 11:25:07 -0400

    ddd

--- blogofdjango/settings.py
+++ blogofdjango/settings.py
@@ -23,9 +23,11 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '@o84i2i%z8tsj%#mhf%_s+an6w%l-r3sx=10jt&aul%j84@zy%'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+   'localhost', '127.0.0.1'
+    ]
 
 
 # Application definition
